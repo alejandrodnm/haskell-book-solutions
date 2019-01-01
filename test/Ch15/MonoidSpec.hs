a91bc2951c6857cf1395946e9082979f9e75ba84
Alejandro Do Nascimento <alejandrodnm@gmail.com>
2019-01-01 22:01:35 +0100

    Lint

--- test/Ch15/MonoidSpec.hs
+++ test/Ch15/MonoidSpec.hs
@@ -7,53 +7,59 @@ import           Test.Hspec
 import           Test.QuickCheck
 
 type TrivAssoc =
-  Trivial -> Trivial -> Trivial -> Bool
+    Trivial -> Trivial -> Trivial -> Bool
 
 instance Arbitrary Trivial where
-  arbitrary = return Trivial
+    arbitrary = return Trivial
 
 type IdentityAssoc =
-  Identity String -> Identity String -> Identity String -> Bool
+    Identity String -> Identity String -> Identity String -> Bool
 
 instance Arbitrary a => Arbitrary (Identity a) where
-  arbitrary = Identity <$> arbitrary
+    arbitrary = Identity <$> arbitrary
 
-type TwoAssoc =
-  Two String (Identity String) ->
-    Two String (Identity String) ->
-      Two String (Identity String) -> Bool
+type TwoAssoc
+    = Two String (Identity String)
+   -> Two String (Identity String)
+   -> Two String (Identity String)
+   -> Bool
 
 instance (Arbitrary a, Arbitrary b) => Arbitrary (Two a b) where
   arbitrary = liftA2 Two arbitrary arbitrary
 
-type OrAssoc =
-  Or String (Identity String) ->
-    Or String (Identity String) ->
-      Or String (Identity String) -> Bool
+type OrAssoc
+    = Or String (Identity String)
+   -> Or String (Identity String)
+   -> Or String (Identity String)
+   -> Bool
 
 instance (Arbitrary a, Arbitrary b) => Arbitrary (Or a b) where
-  arbitrary = do
-    a <- arbitrary
-    b <- arbitrary
-    elements [Fst a, Snd b]
+    arbitrary = do
+        a <- arbitrary
+        b <- arbitrary
+        elements [Fst a, Snd b]
 
 type ValidationAssoc
-   = Validation String String
-  -> Validation String String
-  -> Validation String String
-  -> Bool
+    = Validation String String
+   -> Validation String String
+   -> Validation String String
+   -> Bool
 
 instance (Arbitrary a, Arbitrary b) => Arbitrary (Validation a b) where
-  arbitrary = do
-    a <- arbitrary
-    b <- arbitrary
-    elements [Ch15.Monoid.Failure a,  Ch15.Monoid.Success b]
+    arbitrary = do
+        a <- arbitrary
+        b <- arbitrary
+        elements [Ch15.Monoid.Failure a,  Ch15.Monoid.Success b]
 
 semigroupAssoc :: (Eq m, Semigroup m) => m -> m -> m -> Bool
 semigroupAssoc a b c =
-  (a <> (b <> c)) == ((a <> b) <> c)
+    (a <> (b <> c)) == ((a <> b) <> c)
 
-type CombineAssoc = Fun Int (Sum Int) -> Fun Int (Sum Int) -> Fun Int (Sum Int) -> Bool
+type CombineAssoc
+    = Fun Int (Sum Int)
+   -> Fun Int (Sum Int)
+   -> Fun Int (Sum Int)
+   -> Bool
 
 semigroupFunAssoc :: (Eq b, Semigroup m)
                   => (Fun a b -> m)
@@ -78,16 +84,19 @@ monoidRightIdentity a = (a <> mempty) == a
 
 spec :: Spec
 spec =
-  describe "Semi groups" $ do
-    it "Trivial" $
-      property (semigroupAssoc :: TrivAssoc)
-    it "Identity" $
-      property (semigroupAssoc :: IdentityAssoc)
-    it "Two" $
-      property (semigroupAssoc :: TwoAssoc)
-    it "Or" $
-      property (semigroupAssoc :: OrAssoc)
-    it "Combine" $
-      property (semigroupFunAssoc (Combine . applyFun) unCombine :: Int -> CombineAssoc)
-    it "Validation" $
-      property (semigroupAssoc :: ValidationAssoc)
+    describe "Semi groups" $ do
+        it "Trivial" $
+            property (semigroupAssoc :: TrivAssoc)
+        it "Identity" $
+            property (semigroupAssoc :: IdentityAssoc)
+        it "Two" $
+            property (semigroupAssoc :: TwoAssoc)
+        it "Or" $
+            property (semigroupAssoc :: OrAssoc)
+        it "Combine" $
+            property
+                (semigroupFunAssoc (Combine . applyFun) unCombine
+                    :: Int -> CombineAssoc
+                )
+        it "Validation" $
+            property (semigroupAssoc :: ValidationAssoc)
